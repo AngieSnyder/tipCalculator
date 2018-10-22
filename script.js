b21a97eba4c0fba8aeade08fd0eeb05fc52bb264
AngieSnyder <Angie.Snyder@protonmail.com>
2018-10-21 20:19:06 -0400

    inital commit

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('container').onchange = function() {
+    let bill = Number(document.getElementById('mealTotal').value);
+    let tipPercent = document.getElementById('tipInput').value;
+    let split = document.getElementById('splitInput').value;
+    let tipValue = bill*(tipPercent / 100);
+    let newBillEach = (bill + tipValue) / split;
+    let tipEach = tipValue / split;
+
+    
+    document.getElementById('tipOutput').innerHTML = tipPercent + "%";
+    document.getElementById('splitOutput').innerHTML = split;
+    document.getElementById('tipTotal').innerHTML = "$" + tipValue.toFixed(2);
+    document.getElementById('tipAmt').innerHTML = "$" + tipEach.toFixed(2);
+    document.getElementById('billTotal').innerHTML = "$" + newBillEach.toFixed(2);
+}
