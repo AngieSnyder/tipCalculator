d46d52b7693491860662da9d3df2cb04891e7485
AngieSnyder <Angie.Snyder@protonmail.com>
2018-10-22 09:58:28 -0400

    inital commit

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-document.getElementById('container').onchange = function() {
+document.getElementById('container').onchange = function () {
     let bill = Number(document.getElementById('mealTotal').value);
     let tipPercent = document.getElementById('tipInput').value;
     let split = document.getElementById('splitInput').value;
-    let tipValue = bill*(tipPercent / 100);
+    let tipValue = bill * (tipPercent / 100);
+    let newBillTotal = bill + tipValue;
     let newBillEach = (bill + tipValue) / split;
     let tipEach = tipValue / split;
 
-    
+
     document.getElementById('tipOutput').innerHTML = tipPercent + "%";
     document.getElementById('splitOutput').innerHTML = split;
     document.getElementById('tipTotal').innerHTML = "$" + tipValue.toFixed(2);
     document.getElementById('tipAmt').innerHTML = "$" + tipEach.toFixed(2);
-    document.getElementById('billTotal').innerHTML = "$" + newBillEach.toFixed(2);
+    document.getElementById('billTotal').innerHTML = "$" + newBillTotal.toFixed(2);
+    document.getElementById('billTotalPP').innerHTML = "$" + newBillEach.toFixed(2);
 }
